ba582ee1ccaa027874f8b059b033a810730a9b35
Mariaxuf22 <97722846+Mariaxuf22@users.noreply.github.com>
2023-05-10 11:01:36 +0300

    Add files via upload

---
+++ lab2/lab2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char inputChar = Console.ReadKey().KeyChar;
+            int inputCode = (int)inputChar;
+            int outputCode = (inputCode + 1) % 256;
+            char outputChar = (char)outputCode;
+
+            switch (inputCode % 4)
+            {
+                case 0:
+                    Console.Write("(" + outputChar + ")");
+                    break;
+                case 1:
+                    Console.Write("[" + outputChar + "]");
+                    break;
+                case 2:
+                    Console.Write("<" + outputChar + ">");
+                    break;
+                case 3:
+                    Console.Write("{" + outputChar + "}");
+                    break;
+            }
+        }
+    }
+}
